c3c43ce8096327df8e1eb124c8c82e80b74ca1a0
Marek-kyo <158253274+Marek-kyo@users.noreply.github.com>
2025-07-03 12:33:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
 const CACHE_NAME = "inventory-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/dexie/3.2.2/dexie.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/peerjs/1.4.7/peerjs.min.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
